d2c45dd9cf5b3a58fb1a072bc6108848454e49b6
Neil Stephens <dearknarl@gmail.com>
2023-03-26 10:27:57 +1100

    copywrite headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/*	MiniPlex
+ *
+ *	Copyright (c) 2023: Neil Stephens
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+
 #include "CmdArgs.h"
 #include "MiniPlex.h"
 #include <asio.hpp>
@@ -16,11 +33,8 @@ int main(int argc, char* argv[])
 	for(int i = 0; i < Args.ConcurrencyHint.getValue(); i++)
 		threads.emplace_back([&](){ IOC.run(); });
 
-	asio::signal_set signals(IOC);
-	signals.add(SIGINT);
-	signals.add(SIGTERM);
+	asio::signal_set signals(IOC,SIGINT,SIGTERM,SIGQUIT);
 	signals.add(SIGABRT);
-	signals.add(SIGQUIT);
 	signals.async_wait([&](asio::error_code err, int)
 	{
 		if(!err)
